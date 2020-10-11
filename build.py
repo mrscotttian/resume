365bf1d2d01e12d4a3faf957de6ecdbd58b17443
scotttian <scotttian2014@outlook.com>
2020-10-11 22:38:54 +0800

    markdown to html scripts

--- build.py
+++ build.py
@@ -1,2 +1,14 @@
 import markdown
 
+SOURCE_PATH = 'resume.md'
+OUTPUT_PATH = 'dist/resume.html'
+
+sourceFile = open(SOURCE_PATH, 'r', encoding='utf-8')
+outputFile = open(OUTPUT_PATH, 'w', encoding='utf-8')
+
+sourceString = sourceFile.read()
+htmlSting = markdown.markdown(sourceString)
+outputFile.write(htmlSting)
+
+outputFile.close()
+sourceFile.close()
---
+++ resume.md
@@ -0,0 +1,1 @@
+#Scott Tian 田书炜
